4c94b36debac32e4ac24cd8a22853492048454a6
Anna Brandenberger <anna.cimf@gmail.com>
2021-01-30 05:46:34 +0000

    works

--- script.js
+++ script.js
@@ -9,44 +9,60 @@ let material;
 
 window.addEventListener("load", init);
 
-let rot = 0;
+let cameraRot = 0;
 
 // curve constants
-const numPoints = 200;
+const numPoints = 100;
 const dt = 0.1; // e^{0A} e^{dt A} e^{2dt A}
 const A = math.matrix(
     [
-        [1, 0, 1],
-        [2, 3, 1],
-        [-1, -1, 1]
+        [0, 0, 1],
+        [0, 0, -1],
+        [-1, 1, 0]
     ]
 );
 
-const v = math.multiply(0.1, math.matrix([1, 2, 3]));
+let vs = []
 
-console.log(math.multiply(v, 3));
-console.log(v);
+let n = 1;
+for (let i = -n; i <= n; i+=1) {
+  for (let j = -n; j <= n; j+=1) {
+    for (let k = -n; k <= n; k+=1) {
+      vs.push(math.multiply(20, math.matrix([i, j, k])));
+    }
+  }
+}
 
 function computeCurve() {
-    const res = [];
+    const curves = [];
 
-    for (let i = 0; i < numPoints; i++) {
-        let u = math.multiply(math.expm(math.multiply(i * dt, A)), v);
-        let w = new THREE.Vector3();
-        w.x = math.subset(u, math.index(0));
-        w.y = math.subset(u, math.index(1));
-        w.z = math.subset(u, math.index(2));
-        res.push(w);
+    for (let v of vs) {
+        let curve = [];
+        for (let i = -numPoints; i < numPoints; i++) {
+            let u = math.multiply(math.expm(math.multiply(i * dt, A)), v);
+            let w = new THREE.Vector3();
+            w.x = math.subset(u, math.index(0));
+            w.y = math.subset(u, math.index(1));
+            w.z = math.subset(u, math.index(2));
+            curve.push(w);
+        }
+        curves.push(curve);
     }
 
     // returns a bunch of THREE.vector3s 
-    return res;
+    return curves;
 }
 
 function init() {
     // all three.js code goes here! :)) 
 
     const scene = new THREE.Scene();
+    
+    const color = 'lightblue';
+    const near = 10;
+    const far = 3000;
+    scene.fog = new THREE.Fog(color, near, far);
+
     const camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 5000);
     const geometry = new THREE.Geometry();
 
@@ -58,21 +74,21 @@ function init() {
         // make this more transparent ?
     });
 
-    let linePoints = computeCurve();
-    console.log(linePoints[10]);
-    const lineGeometry = new THREE.BufferGeometry().setFromPoints(linePoints);
-    const lines = new THREE.Line(lineGeometry, lineMaterial);
-
-    scene.add(lines);
+    let lines = computeCurve();
+    lines.forEach((linePoints) => {
+        const lineGeometry = new THREE.BufferGeometry().setFromPoints(linePoints);
+        const curve = new THREE.Line(lineGeometry, lineMaterial);
+        scene.add(curve);
+    });
 
     camera.position.x = 0;
-    camera.position.y = 0;
+    camera.position.y = 30;
     camera.position.z = 100;
     camera.lookAt(new THREE.Vector3(0, 0, 0));
 
     function render() {
-        rot += .75;
-        const radian = (rot * Math.PI) / 180;
+        cameraRot += .5;
+        const radian = (cameraRot * Math.PI) / 180;
         camera.position.x = 1000 * Math.sin(radian);
         camera.position.z = 1000 * Math.cos(radian);
         camera.lookAt(new THREE.Vector3(0, 0, 0));
@@ -83,7 +99,7 @@ function init() {
     }
 
     renderer.render(scene, camera);
-    
+
     render();
 
 
